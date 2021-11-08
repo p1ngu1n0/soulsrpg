b2184096937dae7e72250a96f2cc162e464f9d67
p1ngu1n0 <36717227+p1ngu1n0@users.noreply.github.com>
2021-11-08 19:04:09 +0100

    main

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-import soulsrpg
+#!/usr/bin/env python3.10
+# -*- coding: utf-8 -*-
 # NOTE(cdecompilador): Need this long path, otherwise tests need to import also OpenGL
+# __autor__ = ["p1ngu1n0", "cdecompilador"]
+
 from soulsrpg.utils import Game
+
 
 def main() -> int:
     Game("Hello pygame", (500, 500), None).run()
     return 0
+
 
 if __name__ == "__main__":
     exit(main())
