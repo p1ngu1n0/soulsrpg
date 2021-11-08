da33413779782b88eab2e0f042cff445f66b93c5
p1ngu1n0 <36717227+p1ngu1n0@users.noreply.github.com>
2021-11-08 20:19:57 +0100

    main

--- soulsrpg/player.py
+++ soulsrpg/player.py
@@ -1,5 +1,7 @@
+from ctypes import set_last_error
 import pygame
-from pygame.constants import K_LEFT, K_RIGHT
+from pygame import key
+from pygame.constants import K_DOWN, K_LEFT, K_RIGHT, K_UP
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, **stats) -> None:
@@ -10,6 +12,7 @@ class Player(pygame.sprite.Sprite):
         self.live = stats.get("live", int)
         self.plr_x = stats.get("plr_x", int)
         self.plr_y = stats.get("plr_y", int)
+        self.speed = stats.get("speed", 3)
         self.stamina = stats.get("stamina", int)
         self.image = pygame.image.load("assets/cara.png").convert()
         self.rect = self.image.get_rect()
@@ -17,9 +20,13 @@ class Player(pygame.sprite.Sprite):
     def update(self) -> None:
         keys = pygame.key.get_pressed()
         if keys[K_RIGHT]:
-            self.rect.centerx += 1
+            self.rect.centerx += self.speed
         elif keys[K_LEFT]:
-            self.rect.centerx -= 1
+            self.rect.centerx -= self.speed
+        elif keys[K_UP]:
+            self.rect.centery -= self.speed
+        elif keys[K_DOWN]:
+            self.rect.centery += self.speed
 
     def draw(self, screen):
         screen.blit(self.image, (self.plr_x, self.plr_y))
