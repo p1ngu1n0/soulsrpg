0ac5db6f88029ad2c9abc9e7e63b35d6eb4c7722
cdecompilador <nyagouno@gmail.com>
2021-11-08 14:04:15 +0100

    llego el pingas

--- soulsrpg/soulsrpg.py
+++ soulsrpg/soulsrpg.py
@@ -1,5 +1,18 @@
+import pygame
+from pygame.locals import *
+from typing import NamedTuple, Tuple, overload
+
+class Game(object):
+    window_surface: pygame.Surface
+    
+    def __init__(self, title: str, size: Tuple[int, int]):
+        pygame.init()
+        self.window_surface = pygame.display.set_mode(size, DOUBLEBUF | OPENGL)
+        pygame.display.set_caption(title)
+
+
 def main() -> int:
-    print("Hello from soulsrpg")
+    g = Game("Hello pygame", (500, 500))
     return 0
 
 if __name__ == "__main__":
