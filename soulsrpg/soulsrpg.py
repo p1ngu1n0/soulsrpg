6dd52b045b9f6fe3f86330d259d5c27b6f4802f9
cdecompilador <nyagouno@gmail.com>
2021-11-08 14:35:21 +0100

    UwU 2

--- soulsrpg/soulsrpg.py
+++ soulsrpg/soulsrpg.py
@@ -1,20 +1,36 @@
 import pygame
 from pygame.locals import *
 from typing import NamedTuple, Tuple, overload
+from abc import ABC, abstractmethod
+
+class Scene(ABC):
+    @abstractmethod
+    def update(self, dt: float):
+        pass
+
+    @abstractmethod
+    def render(self):
+        pass
 
 class Game(object):
     window_surface: pygame.Surface
+    running: bool
     
     def __init__(self, title: str, size: Tuple[int, int]):
+        self.running = True
+
         pygame.init()
         self.window_surface = pygame.display.set_mode(size, DOUBLEBUF | OPENGL)
         pygame.display.set_caption(title)
 
-"""
-def main() -> int:
-    g = Game("Hello pygame", (500, 500))
-    return 0
+    def run(self):
+        while self.running:
+            self.update()
+            self.draw()
+            running = False
 
-if __name__ == "__main__":
-    exit(main())
-"""
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
